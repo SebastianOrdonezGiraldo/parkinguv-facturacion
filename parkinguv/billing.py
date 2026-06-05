394794f3ec5136387aa371ccec50be22b006ef17
Sebastian <sebastian789go@gmail.com>
2026-06-05 17:37:44 -0500

    feat: charge hourly fractions after grace period

--- parkinguv/billing.py
+++ parkinguv/billing.py
@@ -1,4 +1,8 @@
+from math import ceil
+
+
 def calculate_fee(minutes: int, vip: bool = False) -> int:
     if minutes <= 30:
         return 0
-    return 500
+    billable_hours = ceil((minutes - 30) / 60)
+    return billable_hours * 500
